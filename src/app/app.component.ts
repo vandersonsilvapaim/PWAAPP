180053a8df962bfafbc570a3b10289c543b269fe
VandersonPaim <vandersonsilvapaim@gmail.com>
2018-11-27 17:00:27 -0200

    enable ivy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwa-app';
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if(confirm("Uma nova versão está disponivel, atualizar ?")) { window.location.reload(); }
+      });
+    }
+  }
+
 }
