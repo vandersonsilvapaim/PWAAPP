2dce41fab66cd9226532e82009f9b1644ed9966b
VandersonPaim <vandersonsilvapaim@gmail.com>
2018-11-28 01:23:58 -0200

    add swpush

--- README.md
+++ README.md
@@ -25,3 +25,10 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+npm install web-push -g
+
+web-push generate-vapid-keys --json
+
+{"publicKey":"BAttKawEzJGDRokn2evHkbIuE-WyjxFEefnFbeagbj19F2JfyCsALOoRZ-k3KfOfhKa20nT21sgyj9XnItGAaL0",
+"privateKey":"msokFaa-jNVm-kzyMNvTfEHkH7VVqM__g28etb1lcxc"}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {SwPush} from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-app';
+  readonly VAPID_PUBLIC_KEY = "BAttKawEzJGDRokn2evHkbIuE-WyjxFEefnFbeagbj19F2JfyCsALOoRZ-k3KfOfhKa20nT21sgyj9XnItGAaL0";
+
+  constructor(private swPush: SwPush,) {
+  }
+
+  //private newsletterService: NewsletterService;
+
+  subscribeToNotifications() {
+
+    //   this.swPush.requestSubscription({
+    //     serverPublicKey: this.VAPID_PUBLIC_KEY
+    //   })
+    //     .then(sub => this.newsletterService.addPushSubscriber(sub).subscribe())
+    //     .catch(err => console.error("Could not subscribe to notifications", err));
+  }
 }
